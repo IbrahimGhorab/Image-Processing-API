d5b5aad65b48f634e7d2dc410acd5c8fee79539d
IbrahimGhorab <ibrahimabdallah81@gmail.com>
2022-11-23 15:19:50 +0200

    Removed dist

--- dist/utils/data.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.resizedImageList = exports.imageList = void 0;
-exports.imageList = ['1', '2', '3', '4', '5'];
-exports.resizedImageList = [];
